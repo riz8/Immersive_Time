62b1290e559337e3d4f5b5207583096995883218
Rickard Zachrisson <rizach8@gmail.com>
2023-06-17 21:07:19 +0200

    Print Awake on awake
    Minimal code for basic module

--- Plugin.cs
+++ Plugin.cs
@@ -3,7 +3,20 @@ using BepInEx;
 
 namespace ImmersiveTime
 {
+    [BepInPlugin(GUID, NAME, VERSION)]
     public class ImmersiveTime : BaseUnityPlugin
     {
+        public const string GUID = "rob.one.immersive_time";
+        public const string NAME = "ImmersiveTime";
+        public const string VERSION = "0.1";
+
+        public static ImmersiveTime Instance;
+
+        internal void Awake()
+        {
+            Instance = this;
+
+            Instance.Logger.LogDebug("Awake");
+        }
     }
 }
