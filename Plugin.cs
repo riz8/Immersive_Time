5a09d30987c5021278fa54e7d58e719ed2665ad9
Rickard Zachrisson <rizach8@gmail.com>
2023-06-18 12:07:53 +0200

    Added TranslationLabels, Timeslot struct, Find operation for TimeSlots
    Added TranslationLabels for all timeslots. A Timeslot struct to keep translation label and a predicate to know if its the current timneslot. Looping over the Timeslots with array.find to figure out what timeslot is currently active.

--- Plugin.cs
+++ Plugin.cs
@@ -13,13 +13,39 @@ namespace ImmersiveTime
 
         public static ImmersiveTime Instance;
 
-        private string morning      = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.Morning.ToString());      // >= 5 && < 12
-        private string afterNoon    = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.AfterNoon.ToString());    // >= 13 && < 18
-        private string evening      = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.Evening.ToString());      // >= 18 && < 22
-        private string night        = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.Night.ToString());        // >= 22 && < 5
-        private string noon         = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.Noon.ToString());
+        
+        private readonly struct TranslationLabels
+        {
+            public static readonly string MORNING   = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.Morning.ToString());      // >= 5 && < 12
+            public static readonly string AFTERNOON = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.AfterNoon.ToString());    // >= 13 && < 18
+            public static readonly string EVENING   = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.Evening.ToString());      // >= 18 && < 22
+            public static readonly string NIGHT     = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.Night.ToString());        // >= 22 && < 5
+            public static readonly string NOON      = LocalizationManager.Instance.GetLoc(EnvironmentConditions.TimeOfDayTimeSlot.Noon.ToString());         // See 1
+        }
 
-        private string timeSlot;
+        private string currentTimeText;
+        private readonly struct TimeSlot
+        {
+            public TimeSlot(string outputText_, Func<bool> predicate_)
+            {
+                outputText = outputText_;
+                predicate = predicate_;
+            }
+
+            public bool MatchesCurrentTime() { return predicate(); }
+            public string OutputText() { return outputText; }
+
+            private readonly string     outputText;
+            private readonly Func<bool> predicate;
+        }
+
+        readonly TimeSlot[] timeSlots = {
+            new TimeSlot(TranslationLabels.MORNING,     () => { return EnvironmentConditions.Instance.IsMorning; }),
+            new TimeSlot(TranslationLabels.AFTERNOON,   () => { return EnvironmentConditions.Instance.IsAfterNoon; }),
+            new TimeSlot(TranslationLabels.EVENING,     () => { return EnvironmentConditions.Instance.IsEvening; }),
+            new TimeSlot(TranslationLabels.NIGHT,       () => { return EnvironmentConditions.Instance.IsNight; }),
+            new TimeSlot(TranslationLabels.NOON,        () => { return EnvironmentConditions.Instance.IsNoon; }) // See 1
+        };
 
         internal void Awake()
         {
@@ -27,8 +53,6 @@ namespace ImmersiveTime
 
             var harmony = new Harmony(GUID);
             harmony.PatchAll();
-
-            timeSlot = Instance.night;
 
             //Instance.Logger.LogDebug("Awake");
         }
@@ -42,13 +66,11 @@ namespace ImmersiveTime
         public class MapDisplay_Show
         {
             [HarmonyPostfix]
-            public static void Postfix(MapDisplay __instance)
+            public static void Postfix(MapDisplay _)
             {
-                if (EnvironmentConditions.Instance.IsMorning)
-                {
-                    Instance.timeSlot = Instance.morning;
-                }
-
+                Instance.currentTimeText = Array.Find(
+                    Instance.timeSlots, timeSlot => timeSlot.MatchesCurrentTime())
+                        .OutputText();
             }
         }
 
@@ -58,9 +80,13 @@ namespace ImmersiveTime
             [HarmonyPostfix]
             public static void Postfix(MapDisplay __instance)
             {
-                __instance.m_timeOfDay.text = Instance.timeSlot;
+                __instance.m_timeOfDay.text = Instance.currentTimeText;
 
             }
         }
     }
 }
+
+// 1.   The source code has a bug using || instead of &&
+//      This means that IsNoon matches anything after 12. Like: (time > 12 || time < 5)
+//      The best counteraction is to validate Noon after all other timeslots
